4e3e1db879db4f03ddd8e59ed684b09aef08c53e
kulotsirosel <36182030+kulotsirosel@users.noreply.github.com>
2018-02-06 12:46:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<!-- saved from url=(0023)file:///F:/_/rosel.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+	</head><body><tittle><b><h1>PERSONAL BACKGROUND</h1></b></tittle>
+
+<p>
+<b>Name:</b> Aliganga,Rosel A.<br>
+<b>Address:</b> Pangdan,City of Naga,Cebu <br>
+<b>Age:</b> 19 years old <br>
+<b>Gender:</b> Female <br>
+<b>Email:</b> rosel.aliganga898@gmail.com <br>
+<b>Date of Birth:</b> December 8,1998 <br>
+<b>Place of Birth</b> Minglanilla District Hospital <br>
+<b>Mother's Name:</b> Luzviminda A. Aliganga <br>
+<b>Father's Name:</b> Dioscoro A. Aliganga <br>
+
+</p><hr><b><h1> EDUCATIONAL ATTAINMENT </h1></b>
+
+<table border="1">
+	<tbody><tr>
+		<th>Level:</th>
+		<th>School:</th>
+		<th>Address:</th>
+		<th>Academic Year:</th>
+	</tr>
+	<tr>
+		<td>Elementary</td>
+		<td> Pangdan Elementary School</td>
+		<td>Pangdan,City of Naga</td>
+		<td>2010-2011</td>
+	</tr>
+	<tr>
+		<td>High School</td>
+		<td> Naga National High School</td>
+		<td>West Poblacion,City of Naga</td>
+		<td>2014-2015</td>
+	</tr>
+	<tr>
+		<td>College</td>
+		<td> Cebu Technological University-Naga Extension Campus</td>
+		<td>CentralPoblacion,City of Naga</td>
+		<td>2017-present</td>
+	</tr>
+	
+</tbody></table>
+
+<br>
+<hr><b><h1> BIOGRAPHY </h1></b>
+<p>
+	Hi my name is Rosel Alferez Aliganga. I live in Pangdan,City og Naga,Cebu. Im 19 years old and have four (4) brothers named Ruel,Rusty John,Rodney and Rasheed.
+
+ I live in Pangdan since I was five (5) years old where my father's parents lived. My grandfather at that time was sick and my mother was asked to taked good care
+ of him. So that was the reason why we transfer in Pangdan. But sad to say after several weeks my grandfather died. So, my father decided to live in Pangdan permanently  and so my mother.
+
+Before, I was called Kiray becaused I was looked alike of Kiray Celis, even now some of my friends still called me that nickname. I get mad when someone called me that name, I am already 
+immuned.
+Others, call me "kulot" because of my curly hair and messy hair. My skin color and eyes is brown.
+
+First,I'd like to finish my studies to reach my goals in life and my goals for my family. They're been my inspiration since I was kid but I don't want to follow the way they discipline us,
+before
+Eventhough,they were like that before I already understand the  reasons why they do that to us. Second, I want to develops my skills in Photography and maybe someday I will create my own
+business. 
+about photography.   
+  
+</p></body></html>
